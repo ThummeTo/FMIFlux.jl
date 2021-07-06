dd8d30e7c916eacc21a2b9b3dd635217b51a3c97
Documenter.jl <documenter@juliadocs.github.io>
2021-07-06 16:46:51 +0000

    build based on f32e8cf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FMIFlux.jl-Documentation","page":"Home","title":"FMIFlux.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Pages = [\"index.md\"]","category":"page"},{"location":"","page":"Home","title":"Home","text":"The documentation for FMIFlux is currently under construction and will be available soon.","category":"page"},{"location":"#FMIFlux.jl-Index","page":"Home","title":"FMIFlux.jl Index","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
