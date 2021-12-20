b6a99ea708b941815bf24661c3894607a1bab497
ThummeTo <83663542+ThummeTo@users.noreply.github.com>
2021-12-20 13:26:05 +0100

    Bug-fix (ignored start state for ME-FMUs) (#16)
    * Bug-fix (ignored start state for ME-FMUs)
    
    * Adapted version
    
    * DiffEqBase issue
    
    * RootFindOptions now in SciMLBase
    
    * SciML Dependencies

--- src/FMI_neural.jl
+++ src/FMI_neural.jl
@@ -15,6 +15,8 @@ using DiffEqFlux: ODEFunction, basic_tgrad, ODEProblem, ZygoteVJP, Interpolating
 import ForwardDiff
 import Optim
 
+import SciMLBase: RightRootFind
+
 include("FMI2_neural.jl")
 
 """
@@ -277,7 +279,7 @@ Via optional argument `reset`, the FMU is reset every time evaluation is started
 Via optional argument `setup`, the FMU is set up every time evaluation is started (default=`true`).
 Via optional argument `rootSearchInterpolationPoints`, the number of interpolation points during root search before event-handling is controlled (default=`100`). Try to increase this value, if event-handling fails with exception.
 """
-function (nfmu::ME_NeuralFMU)(x_start::Array{<:Real}, 
+function (nfmu::ME_NeuralFMU)(x_start::Union{Array{<:Real}, Nothing} = nothing, 
                               t_start::Real = nfmu.tspan[1], 
                               t_stop = nfmu.tspan[end]; 
                               reset::Bool=true,
@@ -288,13 +290,12 @@ function (nfmu::ME_NeuralFMU)(x_start::Array{<:Real},
     if reset
         fmiReset(nfmu.fmu)
     end
+
     if setup
         fmiSetupExperiment(nfmu.fmu, t_start)
         fmiEnterInitializationMode(nfmu.fmu)
         fmiExitInitializationMode(nfmu.fmu)
     end
-
-    x0 = x_start # [t_start, x_start...]
 
     c = nfmu.fmu.components[end]
     tspan = getfield(nfmu.neuralODE,:tspan)
@@ -306,6 +307,7 @@ function (nfmu::ME_NeuralFMU)(x_start::Array{<:Real},
     callbacks = []
     sense = nothing
     savedValues = nothing
+    x0 = nothing
 
     saving = (length(nfmu.recordValues) > 0)
 
@@ -319,7 +321,11 @@ function (nfmu::ME_NeuralFMU)(x_start::Array{<:Real},
         end
 
         # First evaluation of the FMU
-        x0 = fmi2GetContinuousStates(c)
+        if x_start == nothing
+            x0 = fmi2GetContinuousStates(c)
+        else 
+            x0 = x_start
+        end
         x0_nom = fmi2GetNominalsOfContinuousStates(c)
 
         fmi2SetContinuousStates(c, x0)
@@ -345,7 +351,7 @@ function (nfmu::ME_NeuralFMU)(x_start::Array{<:Real},
             eventCb = VectorContinuousCallback((out, x, t, integrator) -> condition(nfmu, out, x, t, integrator),
                                             (integrator, idx) -> affectFMU!(nfmu, integrator, idx),
                                             Int64(nfmu.fmu.modelDescription.numberOfEventIndicators);
-                                            rootfind=DiffEqBase.RightRootFind,
+                                            rootfind=RightRootFind,
                                             save_positions=(false, false),
                                             interp_points=rootSearchInterpolationPoints) 
             push!(callbacks, eventCb)
