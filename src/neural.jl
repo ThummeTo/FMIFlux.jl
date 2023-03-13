8f1e2c0b3adbdecff02aedb84a21c246187f937b
ThummeTo <83663542+ThummeTo@users.noreply.github.com>
2023-03-13 08:47:22 +0100

    Update neural.jl

--- src/neural.jl
+++ src/neural.jl
@@ -1399,7 +1399,7 @@ A function analogous to Flux.train! but with additional features and explicit pa
 - `proceed_on_assert` a boolean that determins wheater to throw an ecxeption on error or proceed training and just print the error
 - `numThreads` [WIP]: an integer determining how many threads are used for training (how many gradients are generated in parallel)
 """
-function train!(loss, params::Union{Flux.Params, Zygote.Params}, data, optim::Flux.Optimise.AbstractOptimiser; gradient::Symbol=:ForwardDiff, cb=nothing, chunk_size::Union{Integer, Symbol}=:auto_fmiflux, printStep::Bool=false, proceed_on_assert::Bool=false, numThreads::Integer=1)
+function train!(loss, params::Union{Flux.Params, Zygote.Params}, data, optim::Flux.Optimise.AbstractOptimiser; gradient::Symbol=:ForwardDiff, cb=nothing, chunk_size::Union{Integer, Symbol}=:auto_forwarddiff, printStep::Bool=false, proceed_on_assert::Bool=false, numThreads::Integer=1)
 
     if chunk_size == :auto_fmiflux
         ram = ceil(Int, Sys.total_memory()/(2^30))
