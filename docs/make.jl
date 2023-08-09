318c47c31ca5ce78427c42762db840e8bd2c9e42
TT <tobias.thummerer@informatik.uni-augsburg.de>
2023-08-09 10:38:34 +0200

    typo crashed docu

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,7 @@ makedocs(sitename="FMIFlux.jl",
                     "Simple CS-NeuralFMU" => "examples/simple_hybrid_CS.md"
                     "Simple ME-NeuralFMU" => "examples/simple_hybrid_ME.md"
                     "Advanced ME-NeuralFMU" => "examples/advanced_hybrid_ME.md"
-                    "JuliaCon 2023" => "examples/julicon_2023.md"
+                    "JuliaCon 2023" => "examples/juliacon_2023.md"
                     "MDPI 2022" => "examples/mdpi_2022.md"
                     "Modelica Conference 2021" => "examples/modelica_conference_2021.md"
             ]
