44ff5e7ce5020bac794907fc89b00e91d55eee7a
Julia Gundermann <julia.gundermann@esi-group.com>
2023-11-21 19:32:35 +0100

    Correct package load in supported_sensitivities.jl (use FMIImport and Flux as dependencies of FMIFlux package)

--- test/supported_sensitivities.jl
+++ test/supported_sensitivities.jl
@@ -3,10 +3,10 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
-using Flux
+using FMIFlux.Flux
 using FMIFlux.DifferentialEquations
 using FMIZoo
-using FMIImport
+using FMIFlux.FMIImport
 import Random 
 Random.seed!(5678);
 
